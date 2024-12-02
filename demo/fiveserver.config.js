591006fb555b6c2f929c03df4dc085d8f131ce7d
NimrodLobozar <nimrod.lobozar@gmail.com>
2024-12-02 16:01:04 +0100

    Initial commit

---
+++ demo/fiveserver.config.js
@@ -0,0 +1,4 @@
+// fiveserver.config.js
+module.exports = {
+    php: "C:\\wamp64\\bin\\php\\php8.3.11\\php.exe"   // Windows
+}
---
+++ demo/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Demo</title>
+</head>
+
+<body>
+    <?php
+    $name = "Dark Matter";
+    $read = true;
+
+    if ($read) {
+        $message = "You have read $name";
+    } else {
+        $message = "You have NOT read $name";
+    }
+    ?>
+    <h1>
+        <?= $message; ?>
+    </h1>
+
+</body>
+<script src="fiveserver.config.js"></script>
+
+</html>
